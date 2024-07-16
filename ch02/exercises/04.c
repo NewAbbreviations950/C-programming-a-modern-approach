8e56223641fc0286ccb1b50f3d497fbce029bb4f
Avex <104692825+NewAbbreviations950@users.noreply.github.com>
2024-07-16 11:41:38 +0200

    Create 04.c

---
+++ ch02/exercises/04.c
@@ -0,0 +1,24 @@
+/* Write a program that declares and several int and float variables without 
+* initialising them then prints their values. Is there a pattern to the values?
+* Usually there isn't.
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+    int b;
+    int c;
+    float d;
+    
+    printf("int a: %d\n", a);
+    printf("int b: %d\n", b);
+    printf("int c: %d\n", c);
+    printf("float d: %f\n", d);
+    
+    return 0;
+}
+
+
+// All returned 0 for me.
