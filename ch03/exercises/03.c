e18df5ff2f1999501e5b9bf10130100ec74fea7e
Avex <104692825+NewAbbreviations950@users.noreply.github.com>
2024-07-22 11:24:36 +0200

    Update 03.c

--- ch03/exercises/03.c
+++ ch03/exercises/03.c
@@ -2,10 +2,10 @@
 * For each of the following pairs of scanf format strings, indicate whether or not the two
 * strings are equivalent. lf they're not. show how they can be distinguished.
 *
-* "%d"       versus " %d"          // equivalent as scanf skips whitespace
-* "%d-%d-%d" versus "%d -%d -%d"   // equivalent as scanf skips whitespace
-* "%f"       versus "%f "          // equivalent as scanf skips whitespace
-* "%f,%f"    versus "%f, %f"       // equivalent as scanf skips whitespace
+* "%d"       versus " %d"          // equivalent but allows the user to input a space before the number
+* "%d-%d-%d" versus "%d -%d -%d"   // equivalent but allows the user to input a space between numbers
+* "%f"       versus "%f "          // equivalent but allows the user to input a space after the number
+* "%f,%f"    versus "%f, %f"       // equivalent but allows the user to input a space before the second number
 */
 
 #include <stdio.h>
