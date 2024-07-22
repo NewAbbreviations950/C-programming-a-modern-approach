1a292cda22b8790d6562927b2f756cf024dc6ecc
Avex <104692825+NewAbbreviations950@users.noreply.github.com>
2024-07-22 13:13:23 +0200

    Create 02.c

---
+++ ch03/projects/02.c
@@ -0,0 +1,38 @@
+/*
+* Write a program that formats product information entered by the user.
+* A session with the program should look like this:
+*
+* Enter an item number: 583
+* Enter unit price: 13.5
+* Enter a purchase date (dd/mm/yyyy): 27/03/2024
+* 
+* Item      Unit        Purchase
+*           price       Date
+* 583       $   13.50   27/03/2024
+*
+* The item number and date should be left justified; the unit price should be right justified.
+* Allow dollar amounts up to $9999.99. Hint: Use tabs to line up the columns.
+*/
+
+#include <stdio.h>
+
+int day, month, year, item;
+float unit_price;
+
+int main (){
+    
+    printf("Enter item number: ");
+    scanf("%d", &item);
+    
+    printf("Enter unit price: ");
+    scanf("%f", &unit_price);
+    
+    printf("Enter purchase date (dd/mm/yyyy): ");
+    scanf("%d/%d/%d", &day, &month, &year);
+    
+    printf("Item\tUnit\tPurchase\n");
+    printf("\tprice\tdate\n");
+    printf("%-d\t$%4.2f\t%d/%d/%d", item, unit_price, day, month, year);
+    
+    return 0;
+}
