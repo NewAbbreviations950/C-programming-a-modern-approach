87807cde177338c3206b394d26abd09d2cc5b708
Avex <104692825+NewAbbreviations950@users.noreply.github.com>
2024-07-23 09:42:11 +0200

    Update 05.c

--- ch03/projects/05.c
+++ ch03/projects/05.c
@@ -22,3 +22,28 @@
  *  the last row give the date of the engraving.)
  */
 
+#include <stdio.h>
+
+int aa, ab, ac, ad, ae, af, ag, ah, ai, aj, ak, al, am, an, ao, ap;
+
+int main() {
+    
+    printf("Enter the numbers from 1 to 16 in any order: \n");
+    scanf("%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d", &aa, &ab, &ac, &ad,
+                                              &ae, &af, &ag, &ah,
+                                              &ai, &aj, &ak, &al,
+                                              &am, &an, &ao, &ap);
+    
+    printf("\nRow sums: %d %d %d %d\n", aa+ab+ac+ad, ae+af+ag+ah, ai+aj+ak+al, am+an+ao+ap);
+    printf("Column sums: %d %d %d %d\n", aa+ae+ai+am, ab+af+aj+an, ac+ag+ak+ao, ad+ah+al+ap);
+    printf("Diagonal sums: %d %d\n", aa+af+ak+ap, ad+ag+aj+am);
+
+    return 0;
+}
+
+// Enter the numbers from 1 to 16 in any order:
+// 2 1 4 3 5 7 6 9 8 11 10 13 12 15 14 16
+//
+// Row sums: 10 27 42 57
+// Column sums: 27 34 34 41
+// Diagonal sums: 35 32
