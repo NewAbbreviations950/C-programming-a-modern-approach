33316fe56c65feaabaac4b3eb7dd4074252541e3
Avex <104692825+NewAbbreviations950@users.noreply.github.com>
2024-07-24 14:05:31 +0200

    Update 04.c

--- ch04/exercises/04.c
+++ ch04/exercises/04.c
@@ -1,5 +1,5 @@
 /*
-* What is the value of each of the following expressions in C89? (Give all possible values if
+* What is the value of each of the following expressions in C99? (Give all possible values if
 * an expression may have more than one value.)
 *
 * (a) 8 / 5   // 1
