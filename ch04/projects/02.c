e74321062e87e620b2c9be3f6af4163230de3f7c
Avex <104692825+NewAbbreviations950@users.noreply.github.com>
2024-07-25 09:58:31 +0200

    Create 02.c

---
+++ ch04/projects/02.c
@@ -0,0 +1,27 @@
+/*
+ * Write a program that asks the user to enter a two-digit number, then prints the number with
+ * its digits reversed. A session with the program should have the following appearance:
+ * 
+ * Enter a two-digit number: 28
+ * The reversal is: 82
+ *
+ * Read the number using %d, then break it into two digits. Hint: if n is an integer, then n % 10
+ * is the last digit in n and n / 10 is n with the last digit removed.
+ *
+ * Extend the program to handle 3 digits. (project 2)
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int original_number;
+    
+    printf("Enter a three digit number: ");
+    scanf("%d", &original_number);
+    
+    printf("The reversal is: %d%d%d\n", ((original_number % 100) % 10), ((original_number % 100) / 10),
+                ((original_number / 10) / 10));
+    
+    return 0;
+}
