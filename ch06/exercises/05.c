c47f7f20bbf33b5b91ac3b072b2e52623d5e9946
Avex <104692825+NewAbbreviations950@users.noreply.github.com>
2025-05-08 08:41:06 +0200

    Create 05.c

---
+++ ch06/exercises/05.c
@@ -0,0 +1,9 @@
+Which one of the following statements is not equivalent to the other two (assuming that the loop bodies are the same)?
+
+(a) while (i < 10) { ... }
+
+(b) for (; i < 10;) { ... }
+
+(c) do { ... } while (i < 10);
+
+(c) is not equivalent to (a) and (b) because it ensures the loop body executes at least once, while the other two loops may not execute at all if the condition is false initially.
