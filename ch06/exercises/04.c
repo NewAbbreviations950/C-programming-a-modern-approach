6ef768abf763a7383ccede0c68c7f8285eb5984d
Avex <104692825+NewAbbreviations950@users.noreply.github.com>
2025-05-08 08:39:01 +0200

    Create 04.c

---
+++ ch06/exercises/04.c
@@ -0,0 +1,9 @@
+Which one of the following statements is not equivalent to the other two (assuming that the loop bodies are the same)?
+
+(a) for (i = 0; i < 10; i++) ...
+
+(b) for (i = 0; i < 10; ++i) ...
+
+(c) for (i = 0; i++ < 10;) ...
+
+(c) is not equivalent to (a) and (b) because the increment operation occurs during the condition check rather than after the loop body executes.
